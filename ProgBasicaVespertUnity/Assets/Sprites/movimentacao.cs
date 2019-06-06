6389f4ef1b5136c7afa6b577cd008fda8c7a8827
alunoscgames <alunoscgames.com>
2019-06-06 16:45:12 -0300

    aula

---
+++ ProgBasicaVespertUnity/Assets/Sprites/movimentacao.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentacao : MonoBehaviour {
+    public float vel = 5;//mudou agora
+    void Update () {
+        Move(Input.GetKey("w") , Input.GetKey("a") , Input.GetKey("s") , Input.GetKey("d"));
+    }
+
+    void Move (bool w , bool a , bool s , bool d)
+    {
+        float speed = vel * Time.deltaTime;//mudou agora
+    Vector3 novaPos = transform.position;
+        if(w){ novaPos += new Vector3 (0, speed, 0); }
+        if(d){ novaPos -= new Vector3(speed , 0, 0); }
+        if(s){ novaPos -= new Vector3(0 , -speed , 0); }
+        if(a){ novaPos += new Vector3(-speed , 0, 0); }
+        transform.position = novaPos;
+    }
+}
