8af235c8a37cd0524da268ef2cbfe39729d3a446
aluno <alunoscgames@gmail.com>
2019-06-06 16:42:08 -0300

    aula

--- ProgBasicaVespertUnity/Assets/Exemplo.cs
+++ ProgBasicaVespertUnity/Assets/Exemplo.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Exemplo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        print("oi");
-    }
-
-    // Update is called once per frame
+    public float vel = 5;
     void Update()
     {
-        
+        //Input.GetKey(KeyCode.W)
+        Move(true, true, false, false);
     }
-}
+
+    void Move(bool w , bool a, bool s, bool d)
+    {
+        float speed = vel * Time.deltaTime; // mudou agora 
+        Vector3 novaPos = transform.position; 
+        if (w){ novaPos += new Vector3(0,speed, 0);  }
+        if (d) { novaPos += new Vector3(speed,0, 0); }
+        if (s) { novaPos += new Vector3(0, -speed, 0); }
+        if (a) { novaPos += new Vector3(-speed, 0, 0); }
+        transform.position = novaPos;
+    }
+}   
