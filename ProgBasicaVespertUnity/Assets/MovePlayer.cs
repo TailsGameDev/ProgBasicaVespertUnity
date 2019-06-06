bb32e65916a762f2953dd75b168d1e69f1b9a476
Caina C. Caldas <cainacc@hotmail.com>
2019-06-06 16:31:14 -0300

    aula 6/6/2019

---
+++ ProgBasicaVespertUnity/Assets/MovePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float vel = 5; // mudou agora
+    void Update()
+    {
+	//Iput.GetKey(KeyCode.W)
+	Move(true, false, false, true);
+
+    }
+
+    void Move(bool w, bool a, bool s, bool d)
+    {
+	float speed = vel * Time.deltaTime; // mudou agora
+	Vector3 novaPos = transform.position;
+	if(w){ novaPos = novaPos + new Vector3(0,speed, 0); }
+	if(s){ novaPos = novaPos + new Vector3(0,-speed, 0); }
+	if(d){ novaPos = novaPos + new Vector3(speed,0, 0); }
+	if(a){ novaPos = novaPos + new Vector3(-speed,0, 0); }
+	transform.position = novaPos;
+    }
+}
+
+        
+	
+        
+	
