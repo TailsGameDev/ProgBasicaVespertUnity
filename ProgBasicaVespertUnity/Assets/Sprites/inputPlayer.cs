4fb50909841b1a485b23da380b2d550b9a42dc91
alunoscgames <alunoscgames.com>
2019-06-13 16:48:51 -0300

    input player aula 13/06/2019

---
+++ ProgBasicaVespertUnity/Assets/Sprites/inputPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputPlayer : MonoBehaviour
+{
+    movimentacao scriptQueAnda;
+   
+    void Start()
+    {
+        scriptQueAnda = GetComponent<movimentacao>();
+    }
+
+   
+    void Update()
+    {
+        bool w = Input.GetKey("w");
+        bool a = Input.GetKey("a");
+        bool s = Input.GetKey("s");
+        bool d = Input.GetKey("d");
+        scriptQueAnda.move(w , a , s , d);
+
+
+
+        scriptQueAnda.move(Input.GetKey("w") , Input.GetKey("a"), Input.GetKey("s"), Input.GetKey("d"));
+    }
+}
--- ProgBasicaVespertUnity/Assets/Sprites/movimentacao.cs
+++ ProgBasicaVespertUnity/Assets/Sprites/movimentacao.cs
@@ -3,19 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class movimentacao : MonoBehaviour {
-    public float vel = 5;//mudou agora
-    void Update () {
-        Move(Input.GetKey("w") , Input.GetKey("a") , Input.GetKey("s") , Input.GetKey("d"));
-    }
 
-    void Move (bool w , bool a , bool s , bool d)
+    public float vel = 5;
+
+   public void move (bool w, bool a, bool s, bool d)
     {
-        float speed = vel * Time.deltaTime;//mudou agora
-    Vector3 novaPos = transform.position;
-        if(w){ novaPos += new Vector3 (0, speed, 0); }
-        if(d){ novaPos -= new Vector3(speed , 0, 0); }
-        if(s){ novaPos -= new Vector3(0 , -speed , 0); }
-        if(a){ novaPos += new Vector3(-speed , 0, 0); }
+        float speed = vel * Time.deltaTime;
+        Vector3 novaPos = transform.position;
+        if(w){ novaPos = novaPos + new Vector3(0, speed, 0); }
+        if(s){ novaPos = novaPos + new Vector3( 0, -speed, 0); }
+        if(d){ novaPos = novaPos + new Vector3(speed, 0 , 0); }
+        if(a){ novaPos = novaPos + new Vector3(-speed , 0, 0); }
         transform.position = novaPos;
     }
 }
