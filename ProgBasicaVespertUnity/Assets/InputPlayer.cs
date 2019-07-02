d767a629f67cebe4884609a8fb8877293c26f00d
Caina C. Caldas <cainacc@hotmail.com>
2019-07-02 13:16:57 -0300

    apostila virou link e alguma coisa que nao lembro mudou na cena

---
+++ ProgBasicaVespertUnity/Assets/InputPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlayer : MonoBehaviour
+{
+    Move scriptQueAnda; 
+
+    void Start()
+    {
+        scriptQueAnda= GetComponent<Move>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	bool w = Input.GetKey("w");
+	bool a = Input.GetKey("a");
+	bool s = Input.GetKey("s");
+	bool d = Input.GetKey("d");
+        scriptQueAnda.move(w,a,s,d);
+    }
+}
--- ProgBasicaVespertUnity/Assets/Move.cs
+++ ProgBasicaVespertUnity/Assets/Move.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public float vel = 5; // mudou agora
+    public float vel = 5;
 
-    void move(bool w, bool a, bool s, bool d)
+    public void move(bool w, bool a, bool s, bool d)
     {
 
-    //print("conta: " +3*5 * 0.33333333);
-	float speed = vel * Time.deltaTime; // mudou agora
-    //print("transform.position: "+transform.position);
-    Vector3 novaPos = transform.position;
+	float speed = vel * Time.deltaTime;
+    	Vector3 novaPos = transform.position;
 	if(w){ novaPos = novaPos + new Vector3(0,speed, 0); }
 	if(s){ novaPos = novaPos + new Vector3(0,-speed, 0); }
 	if(d){ novaPos = novaPos + new Vector3(speed,0, 0); }
